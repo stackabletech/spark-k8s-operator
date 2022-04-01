6c76266a5f487dedcb12ac4c5a466b434e37d48b
Stacky McStackface <github@stackable.de>
2022-04-01 19:32:33 +0000

    Generated commit to update templated files based on rev 8135e36 in stackabletech/operator-templating repo.
    Triggered by:
    Manual run triggered by: soenkeliebau with message [Rerun after fixing accidental hard coded value]

--- scripts/generate-manifests.sh
+++ scripts/generate-manifests.sh
@@ -10,12 +10,11 @@ helm template --output-dir "$tmp" \
               --name-template spark-k8s-operator \
               deploy/helm/spark-k8s-operator
 
-while IFS= read -r -d '' file
-do
-    yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' /dev/stdin < "$file"
-    yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' /dev/stdin < "$file"
-    sed -i '/# Source: .*/d' "$file"
-done <   <(find "$tmp" -type f)
+for file in $tmp/spark-k8s-operator/*/*; do
+  yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' /dev/stdin < "$file"
+  yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' /dev/stdin < "$file"
+  sed -i '/# Source: .*/d' "$file"
+done
 
 cp -r "$tmp"/spark-k8s-operator/*/* deploy/manifests/
 

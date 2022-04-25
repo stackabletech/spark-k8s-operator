9cda52ca061911f10a8b9cfe31fed331f7ee5795
Nikolaus Winter <mail@nikolaus-winter.de>
2022-04-25 22:27:26 +0200

    pull spark-operator log as well after test

--- .ci/integration-tests/hcloud-centos-8/test.sh
+++ .ci/integration-tests/hcloud-centos-8/test.sh
@@ -3,4 +3,5 @@ git clone -b "$GIT_BRANCH" https://github.com/stackabletech/spark-k8s-operator.g
 (cd spark-k8s-operator/ && ./scripts/run_tests.sh)
 exit_code=$?
 ./operator-logs.sh spark-k8s > /target/spark-k8s-operator.log
+./operator-logs.sh spark > /target/spark-operator.log
 exit $exit_code

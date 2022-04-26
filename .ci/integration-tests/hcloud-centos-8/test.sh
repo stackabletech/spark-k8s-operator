4a38298042f6c8d76ba87b92341928de81ab65b6
Nikolaus Winter <mail@nikolaus-winter.de>
2022-04-26 11:28:41 +0000

    integration tests in Jenkins CI (#54)
    ## Description
    
    created integration test files for Jenkins CI
    
    
    
    Co-authored-by: Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>

---
+++ .ci/integration-tests/hcloud-centos-8/test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone -b "$GIT_BRANCH" https://github.com/stackabletech/spark-k8s-operator.git
+(cd spark-k8s-operator/ && ./scripts/run_tests.sh)
+exit_code=$?
+./operator-logs.sh spark-k8s > /target/spark-k8s-operator.log
+./operator-logs.sh spark > /target/spark-operator.log
+exit $exit_code

dd6d507e3423073fd034c306f0515720de146a95
Stacky McStackface <github@stackable.de>
2022-08-16 08:15:43 +0000

    Update templated files to rev 7f69887 (#116)
    Automatically created PR based on commit 7f6988755a98e29ed10f95941f4c281bc5e6fcb6 in stackabletech/operator-templating repo.
    
    Triggered by:
    Daily run triggered

--- scripts/docs_templating.sh
+++ scripts/docs_templating.sh
@@ -7,11 +7,11 @@ set -euo pipefail
 # dependencies
 # pip install jinja2-cli
 
-docs_dir=../docs
-templating_vars_file=$docs_dir/templating_vars.yaml
+docs_dir="$(dirname "$0")/../docs"
+templating_vars_file="$docs_dir/templating_vars.yaml"
 
 # Check if files need templating
-if [[ $(find "$docs_dir" | grep --count .j2\$) -eq "0" ]];
+if [[ -z $(find "$docs_dir" -name '*.j2') ]];
 then
   echo "No files need templating, exiting."
   exit
@@ -30,9 +30,10 @@ then
   echo "$templating_vars_file does not exist, cannot start templating."
 fi
 
-for file in $(find "$docs_dir" | grep .j2\$)
+find "$docs_dir" -name '*.j2' |
+while read -r file
 do
-  new_file_name=$(echo "$file" | sed 's/\(.*\).j2/\1/g')  # cut of the '.j2'
+  new_file_name=${file%.j2} # Remove .j2 suffix
   echo "templating $new_file_name"
   jinja2 "$file" "$templating_vars_file" -o "$new_file_name"
 done

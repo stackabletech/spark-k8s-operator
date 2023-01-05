8362df5bbe439f7c74bd0083d8053570c81cf4eb
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2023-01-05 18:32:04 +0100

    Populate spark config automatically.

--- rust/operator-binary/src/history_controller.rs
+++ rust/operator-binary/src/history_controller.rs
@@ -1,6 +1,10 @@
 use stackable_operator::{
     builder::{ConfigMapBuilder, ContainerBuilder, ObjectMetaBuilder, PodBuilder, VolumeBuilder},
-    commons::product_image_selection::ResolvedProductImage,
+    commons::{
+        product_image_selection::ResolvedProductImage,
+        s3::{InlinedS3BucketSpec, S3AccessStyle},
+        tls::{CaCert, TlsVerification},
+    },
     k8s_openapi::{
         api::{
             apps::v1::{StatefulSet, StatefulSetSpec},
@@ -17,12 +21,12 @@ use stackable_spark_k8s_crd::{
     constants::{
         APP_NAME, HISTORY_CONFIG_FILE_NAME, HISTORY_CONTROLLER_NAME, HISTORY_IMAGE_BASE_NAME,
     },
-    history::SparkHistoryServer,
+    history::{LogFileDirectorySpec::S3, S3LogFileDirectorySpec, SparkHistoryServer},
 };
 use std::{collections::BTreeMap, sync::Arc};
 use std::{collections::HashMap, time::Duration};
 
-use snafu::{ResultExt, Snafu};
+use snafu::{OptionExt, ResultExt, Snafu};
 use stackable_operator::logging::controller::ReconcilerError;
 use strum::{EnumDiscriminants, IntoStaticStr};
 
@@ -65,6 +69,16 @@ pub enum Error {
     ProductConfigValidation {
         source: stackable_spark_k8s_crd::history::Error,
     },
+    #[snafu(display("s3 bucket error"))]
+    S3Bucket {
+        source: stackable_operator::error::Error,
+    },
+    #[snafu(display("tls non-verification not supported"))]
+    S3TlsNoVerificationNotSupported,
+    #[snafu(display("ca-cert verification not supported"))]
+    S3TlsCaVerificationNotSupported,
+    #[snafu(display("missing bucket name for history logs"))]
+    BucketNameMissing,
 }
 
 type Result<T, E = Error> = std::result::Result<T, E>;
@@ -79,6 +93,7 @@ pub async fn reconcile(shs: Arc<SparkHistoryServer>, ctx: Arc<Ctx>) -> Result<Ac
     tracing::info!("Starting reconcile history server");
 
     let resolved_product_image = shs.spec.image.resolve(HISTORY_IMAGE_BASE_NAME);
+    let s3_log_dir = S3LogDir::resolve(&shs, &ctx.client).await?;
 
     // TODO: (RBAC) need to use a dedicated service account, role
 
@@ -122,6 +137,7 @@ pub async fn reconcile(shs: Arc<SparkHistoryServer>, ctx: Arc<Ctx>) -> Result<Ac
                 &resolved_product_image.app_version_label,
                 &rgr,
                 rolegroup_config,
+                &s3_log_dir,
             )?;
             ctx.client
                 .apply_patch(HISTORY_CONTROLLER_NAME, &config_map, &config_map)
@@ -148,16 +164,9 @@ fn build_config_map(
     app_version_label: &str,
     rolegroupref: &RoleGroupRef<SparkHistoryServer>,
     rolegroup_config: &HashMap<PropertyNameKind, BTreeMap<String, String>>,
+    s3_log_dir: &S3LogDir,
 ) -> Result<ConfigMap, Error> {
-    let empty_map = BTreeMap::new();
-
-    let spark_defaults_data = rolegroup_config
-        .get(&PropertyNameKind::File(HISTORY_CONFIG_FILE_NAME.to_owned()))
-        .unwrap_or(&empty_map)
-        .iter()
-        .map(|(k, v)| format!("{k} {v}"))
-        .collect::<Vec<String>>()
-        .join("\n");
+    let spark_config = spark_config(rolegroup_config, s3_log_dir);
 
     let result = ConfigMapBuilder::new()
         .metadata(
@@ -169,7 +178,7 @@ fn build_config_map(
                 .with_recommended_labels(shs.labels(app_version_label, &rolegroupref.role_group))
                 .build(),
         )
-        .add_data(HISTORY_CONFIG_FILE_NAME, spark_defaults_data)
+        .add_data(HISTORY_CONFIG_FILE_NAME, spark_config)
         .build()
         .context(InvalidConfigMapSnafu {
             name: String::from("spark-history-config"),
@@ -288,4 +297,123 @@ fn build_service(
         }),
         status: None,
     })
+}
+
+struct S3LogDir {
+    bucket: InlinedS3BucketSpec,
+    prefix: String,
+}
+
+impl S3LogDir {
+    async fn resolve(
+        shs: &SparkHistoryServer,
+        client: &stackable_operator::client::Client,
+    ) -> Result<S3LogDir, Error> {
+        #[allow(irrefutable_let_patterns)]
+        let (s3bucket, prefix) =
+            if let S3(S3LogFileDirectorySpec { bucket, prefix }) = &shs.spec.log_file_directory {
+                (
+                    bucket
+                        .resolve(client, shs.metadata.namespace.as_deref().unwrap())
+                        .await
+                        .context(S3BucketSnafu)
+                        .ok(),
+                    prefix.clone(),
+                )
+            } else {
+                (None, "".to_string())
+            };
+
+        // Check that a bucket name has been defined
+        s3bucket
+            .as_ref()
+            .and_then(|i| i.bucket_name.as_ref())
+            .context(BucketNameMissingSnafu)?;
+
+        if let Some(conn) = s3bucket.as_ref().and_then(|i| i.connection.as_ref()) {
+            if let Some(tls) = &conn.tls {
+                match &tls.verification {
+                    TlsVerification::None {} => return S3TlsNoVerificationNotSupportedSnafu.fail(),
+                    TlsVerification::Server(server_verification) => {
+                        match &server_verification.ca_cert {
+                            CaCert::WebPki {} => {}
+                            CaCert::SecretClass(_) => {
+                                return S3TlsCaVerificationNotSupportedSnafu.fail()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if let Some(conn) = s3bucket.as_ref().and_then(|i| i.connection.as_ref()) {
+            if conn.tls.as_ref().is_some() {
+                tracing::warn!("The resource indicates S3-access should use TLS: TLS-verification has not yet been implemented \
+            but an HTTPS-endpoint will be used!");
+            }
+        }
+        Ok(S3LogDir {
+            bucket: s3bucket.unwrap(),
+            prefix,
+        })
+    }
+
+    /// Constructs the properties needed for loading event logs from S3.
+    /// These properties are later written in the `HISTORY_CONFIG_FILE_NAME_FULL` file.
+    fn spark_config(&self) -> BTreeMap<String, String> {
+        let mut result = BTreeMap::new();
+
+        result.insert(
+            "spark.history.fs.logDirectory".to_string(),
+            format!(
+                "s3a://{}/{}/",
+                self.bucket.bucket_name.as_ref().unwrap().clone(), // this is guranateed to exist at this point
+                self.prefix
+            ),
+        );
+
+        if let Some(endpoint) = self.bucket.endpoint() {
+            result.insert("spark.hadoop.fs.s3a.endpoint".to_string(), endpoint);
+        }
+
+        if let Some(conn) = self.bucket.connection.as_ref() {
+            if let Some(S3AccessStyle::Path) = conn.access_style {
+                result.insert(
+                    "spark.hadoop.fs.s3a.path.style.access".to_string(),
+                    "true".to_string(),
+                );
+            }
+            if conn.credentials.as_ref().is_some() {
+                result.insert(
+                    "spark.hadoop.fs.s3a.aws.credentials.provider".to_string(),
+                    "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider".to_string(),
+                );
+                result.insert(
+                    "spark.hadoop.fs.s3a.access.key".to_string(),
+                    "PLACEHOLDER_ACCESS_KEY".to_string(),
+                );
+                result.insert(
+                    "spark.hadoop.fs.s3a.secret.key".to_string(),
+                    "PLACEHOLDER_SECRET_KEY".to_string(),
+                );
+            }
+        }
+        result
+    }
+}
+
+fn spark_config(
+    rolegroup_config: &HashMap<PropertyNameKind, BTreeMap<String, String>>,
+    s3_log_dir: &S3LogDir,
+) -> String {
+    let log_dir_settings = s3_log_dir.spark_config();
+
+    // Add user provided configuration. These can overwrite the "log_file_directory" settings.
+    rolegroup_config
+        .get(&PropertyNameKind::File(HISTORY_CONFIG_FILE_NAME.to_owned()))
+        .unwrap_or(&log_dir_settings)
+        .iter()
+        .map(|(k, v)| format!("{k} {v}"))
+        .collect::<Vec<String>>()
+        .join("\n")
 }

00522a51a734ca1d33d3cad0bd919befa3f2b339
Malte Sander <contact@maltesander.com>
2023-01-10 11:47:16 +0000

    Removed deploy crd folder (#190)
    # Description
    
    fixes https://github.com/stackabletech/spark-k8s-operator/issues/8
    
    
    
    Co-authored-by: Malte Sander <malte.sander.it@gmail.com>

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,10 +1,3 @@
-use stackable_operator::crd::CustomResourceExt;
-use stackable_spark_k8s_crd::SparkApplication;
-
-fn main() -> Result<(), stackable_operator::error::Error> {
+fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    SparkApplication::write_yaml_schema("../../deploy/crd/sparkapplication.crd.yaml")?;
-
-    Ok(())
 }

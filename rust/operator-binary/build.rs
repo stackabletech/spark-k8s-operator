3b82aa5a62e8dfcef7536da265365ec2b638eb25
Sönke Liebau <soenke.liebau@stackable.tech>
2023-02-23 11:07:36 +0000

    Changes to be compatible with crate2nix (#208)
    see https://github.com/stackabletech/operator-templating/pull/212 for more details

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,3 +1,13 @@
+use std::path::PathBuf;
+
 fn main() {
-    built::write_built_file().expect("Failed to acquire build-time information");
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").expect("OUT_DIR is required"));
+    built::write_built_file_with_opts(
+        // built's env module depends on a whole bunch of variables that crate2nix doesn't provide
+        // so we grab the specific env variables that we care about out ourselves instead.
+        built::Options::default().set_env(false),
+        "Cargo.toml".as_ref(),
+        &out_dir.join("built.rs"),
+    )
+    .unwrap();
 }

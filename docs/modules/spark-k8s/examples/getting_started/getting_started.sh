ec1e19e0d4c17c40112d19af01b002e2d5017d21
Siegfried Weber <mail@siegfriedweber.net>
2023-11-20 14:24:02 +0100

    docs: Add listener-operator where missing (#308)
    * docs: Add listener-operator where missing
    
    * docs: Add listener-operator where missing

--- docs/modules/spark-k8s/examples/getting_started/getting_started.sh
+++ docs/modules/spark-k8s/examples/getting_started/getting_started.sh
@@ -29,6 +29,7 @@ echo "Installing Operators with Helm"
 # tag::helm-install-operators[]
 helm install --wait commons-operator stackable-dev/commons-operator --version 0.0.0-dev
 helm install --wait secret-operator stackable-dev/secret-operator --version 0.0.0-dev
+helm install --wait listener-operator stackable-dev/listener-operator --version 0.0.0-dev
 helm install --wait spark-k8s-operator stackable-dev/spark-k8s-operator --version 0.0.0-dev
 # end::helm-install-operators[]
 ;;
@@ -38,6 +39,7 @@ echo "installing Operators with stackablectl"
 stackablectl operator install \
   commons=0.0.0-dev \
   secret=0.0.0-dev \
+  listener=0.0.0-dev \
   spark-k8s=0.0.0-dev
 # end::stackablectl-install-operators[]
 ;;

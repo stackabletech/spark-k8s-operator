9d24f34ad2bcbb27cb1e333958d93b528d943997
Malte Sander <malte.sander.it@gmail.com>
2024-10-21 16:06:27 +0200

    adapt to op-rs 0.79.0

--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -72,7 +72,8 @@ async fn main() -> anyhow::Result<()> {
             );
 
             let client =
-                stackable_operator::client::create_client(Some(OPERATOR_NAME.to_string())).await?;
+                stackable_operator::client::initialize_operator(Some(OPERATOR_NAME.to_string()))
+                    .await?;
 
             let ctx = Ctx {
                 client: client.clone(),

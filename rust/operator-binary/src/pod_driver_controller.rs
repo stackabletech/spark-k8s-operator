af19157c8d95456f2f19eff7ad30df4679de09a4
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-10-23 16:07:43 +0200

    fix: invalid objects don't stop the reconciliation

--- rust/operator-binary/src/pod_driver_controller.rs
+++ rust/operator-binary/src/pod_driver_controller.rs
@@ -1,5 +1,8 @@
 use stackable_operator::{
-    client::Client, k8s_openapi::api::core::v1::Pod, kube::runtime::controller::Action,
+    client::Client,
+    k8s_openapi::api::core::v1::Pod,
+    kube::core::{error_boundary, DeserializeGuard},
+    kube::runtime::controller::Action,
     time::Duration,
 };
 use stackable_spark_k8s_crd::{
@@ -35,6 +38,10 @@ pub enum Error {
         source: stackable_operator::client::Error,
         name: String,
     },
+    #[snafu(display("Pod object is invalid"))]
+    InvalidPod {
+        source: error_boundary::InvalidObject,
+    },
 }
 
 type Result<T, E = Error> = std::result::Result<T, E>;
@@ -45,8 +52,14 @@ impl ReconcilerError for Error {
     }
 }
 /// Updates the status of the SparkApplication that started the pod.
-pub async fn reconcile(pod: Arc<Pod>, client: Arc<Client>) -> Result<Action> {
+pub async fn reconcile(pod: Arc<DeserializeGuard<Pod>>, client: Arc<Client>) -> Result<Action> {
     tracing::info!("Starting reconcile driver pod");
+
+    let pod = pod
+        .0
+        .as_ref()
+        .map_err(error_boundary::InvalidObject::clone)
+        .context(InvalidPodSnafu)?;
 
     let pod_name = pod.metadata.name.as_ref().context(PodNameNotFoundSnafu)?;
     let app_name = pod
@@ -94,6 +107,9 @@ pub async fn reconcile(pod: Arc<Pod>, client: Arc<Client>) -> Result<Action> {
     Ok(Action::await_change())
 }
 
-pub fn error_policy(_obj: Arc<Pod>, _error: &Error, _ctx: Arc<Client>) -> Action {
-    Action::requeue(*Duration::from_secs(5))
+pub fn error_policy(_obj: Arc<DeserializeGuard<Pod>>, error: &Error, _ctx: Arc<Client>) -> Action {
+    match error {
+        Error::InvalidPod { .. } => Action::await_change(),
+        _ => Action::requeue(*Duration::from_secs(5)),
+    }
 }

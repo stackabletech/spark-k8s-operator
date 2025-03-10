ccab877afd1a4f70a6d968c9bb17ad6a90096b90
Techassi <git@techassi.dev>
2025-03-10 15:35:19 +0100

    chore: Bump stackable-operator to 0.87.2

--- rust/operator-binary/src/crd/logdir.rs
+++ rust/operator-binary/src/crd/logdir.rs
@@ -182,7 +182,7 @@ impl S3LogDir {
     pub fn history_server_spark_config(&self) -> Result<BTreeMap<String, String>, Error> {
         let connection = &self.bucket.connection;
 
-        let mut config = BTreeMap::from([
+        let config = BTreeMap::from([
             ("spark.history.fs.logDirectory".to_string(), self.url()),
             (
                 "spark.hadoop.fs.s3a.endpoint".to_string(),
@@ -192,14 +192,11 @@ impl S3LogDir {
                 "spark.hadoop.fs.s3a.path.style.access".to_string(),
                 (connection.access_style == S3AccessStyle::Path).to_string(),
             ),
-        ]);
-
-        if let Some(region_name) = connection.region.name() {
-            config.insert(
+            (
                 "spark.hadoop.fs.s3a.endpoint.region".to_string(),
-                region_name.to_string(),
-            );
-        };
+                connection.region.name.clone(),
+            ),
+        ]);
 
         Ok(config)
     }
@@ -220,12 +217,10 @@ impl S3LogDir {
             format!("spark.hadoop.fs.s3a.bucket.{bucket_name}.path.style.access"),
             (connection.access_style == S3AccessStyle::Path).to_string(),
         );
-        if let Some(region_name) = connection.region.name() {
-            result.insert(
-                format!("spark.hadoop.fs.s3a.bucket.{bucket_name}.region"),
-                region_name.to_string(),
-            );
-        };
+        result.insert(
+            format!("spark.hadoop.fs.s3a.bucket.{bucket_name}.region"),
+            connection.region.name.clone(),
+        );
         if let Some(secret_dir) = self.credentials_mount_path() {
             // We don't use the credentials at all here but assume they are available
             result.insert(

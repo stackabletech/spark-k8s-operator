6c033e189866d2259393462c0020edb8a971aaa2
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2025-04-08 11:08:38 +0200

    feat!: Use stackable-telemetry (#547)
    * chore: Replace print_startup_string with structured event
    
    * feat!: Replace log initialization with stackable-telemetry
    
    BREAKING: The log directory variable for the rolling file appender has changed
    
    * chore: Update changelog
    
    * chore: Correct changelog entry
    
    ---------
    
    Co-authored-by: Techassi <git@techassi.dev>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,15 @@ All notable changes to this project will be documented in this file.
 
 ## [Unreleased]
 
+### Changed
+
+- Replace stackable-operator `initialize_logging` with stackable-telemetry `Tracing` ([#547]).
+  - BREAKING: The file log directory was set by `SPARK_K8S_OPERATOR_LOG_DIRECTORY`, and is now set by `ROLLING_LOGS`
+    (or via `--rolling-logs <DIRECTORY>`).
+  - Replace stackable-operator `print_startup_string` with `tracing::info!` with fields.
+
+[#547]: https://github.com/stackabletech/spark-k8s-operator/pull/547
+
 ## [25.3.0] - 2025-03-21
 
 ### Added
--- rust/operator-binary/src/main.rs
+++ rust/operator-binary/src/main.rs
@@ -1,12 +1,12 @@
-use std::sync::Arc;
+use std::{ops::Deref as _, sync::Arc};
 
-use clap::{Parser, crate_description, crate_version};
+use clap::Parser;
 use futures::{StreamExt, pin_mut};
 use history::history_controller;
 use product_config::ProductConfigManager;
 use stackable_operator::{
     YamlSchema,
-    cli::{Command, ProductOperatorRun},
+    cli::{Command, ProductOperatorRun, RollingPeriod},
     k8s_openapi::api::{
         apps::v1::StatefulSet,
         core::v1::{ConfigMap, Pod, Service},
@@ -22,7 +22,8 @@ use stackable_operator::{
     logging::controller::report_controller_reconciled,
     shared::yaml::SerializeOptions,
 };
-use tracing::info_span;
+use stackable_telemetry::{Tracing, tracing::settings::Settings};
+use tracing::{info_span, level_filters::LevelFilter};
 use tracing_futures::Instrument;
 
 use crate::crd::{
@@ -44,6 +45,9 @@ mod spark_k8s_controller;
 mod built_info {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
+
+// TODO (@NickLarsenNZ): Change the variable to `CONSOLE_LOG`
+pub const ENV_VAR_CONSOLE_LOG: &str = "SPARK_K8S_OPERATOR_LOG";
 
 #[derive(Parser)]
 #[clap(about, author)]
@@ -74,21 +78,55 @@ async fn main() -> anyhow::Result<()> {
         Command::Run(ProductOperatorRun {
             product_config,
             watch_namespace,
-            tracing_target,
+            telemetry_arguments,
             cluster_info_opts,
         }) => {
-            stackable_operator::logging::initialize_logging(
-                "SPARK_K8S_OPERATOR_LOG",
-                "spark-k8s",
-                tracing_target,
-            );
-            stackable_operator::utils::print_startup_string(
-                crate_description!(),
-                crate_version!(),
-                built_info::GIT_VERSION,
-                built_info::TARGET,
-                built_info::BUILT_TIME_UTC,
-                built_info::RUSTC_VERSION,
+            let _tracing_guard = Tracing::builder()
+                .service_name("spark-k8s-operator")
+                .with_console_output((
+                    ENV_VAR_CONSOLE_LOG,
+                    LevelFilter::INFO,
+                    !telemetry_arguments.no_console_output,
+                ))
+                // NOTE (@NickLarsenNZ): Before stackable-telemetry was used, the log directory was
+                // set via an env: `SPARK_K8S_OPERATOR_LOG_DIRECTORY`.
+                // See: https://github.com/stackabletech/operator-rs/blob/f035997fca85a54238c8de895389cc50b4d421e2/crates/stackable-operator/src/logging/mod.rs#L40
+                // Now it will be `ROLLING_LOGS` (or via `--rolling-logs <DIRECTORY>`).
+                .with_file_output(telemetry_arguments.rolling_logs.map(|log_directory| {
+                    let rotation_period = telemetry_arguments
+                        .rolling_logs_period
+                        .unwrap_or(RollingPeriod::Never)
+                        .deref()
+                        .clone();
+
+                    Settings::builder()
+                        .with_environment_variable(ENV_VAR_CONSOLE_LOG)
+                        .with_default_level(LevelFilter::INFO)
+                        .file_log_settings_builder(log_directory, "tracing-rs.log")
+                        .with_rotation_period(rotation_period)
+                        .build()
+                }))
+                .with_otlp_log_exporter((
+                    "OTLP_LOG",
+                    LevelFilter::DEBUG,
+                    telemetry_arguments.otlp_logs,
+                ))
+                .with_otlp_trace_exporter((
+                    "OTLP_TRACE",
+                    LevelFilter::DEBUG,
+                    telemetry_arguments.otlp_traces,
+                ))
+                .build()
+                .init()?;
+
+            tracing::info!(
+                built_info.pkg_version = built_info::PKG_VERSION,
+                built_info.git_version = built_info::GIT_VERSION,
+                built_info.target = built_info::TARGET,
+                built_info.built_time_utc = built_info::BUILT_TIME_UTC,
+                built_info.rustc_version = built_info::RUSTC_VERSION,
+                "Starting {description}",
+                description = built_info::PKG_DESCRIPTION
             );
 
             let client = stackable_operator::client::initialize_operator(

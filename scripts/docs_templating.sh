c04a3d933ad86f0c1082b3a08cdf230b4a964c31
Stacky McStackface <github@stackable.de>
2026-07-16 10:15:48 +0000

    chore: Generated commit to update templated files since the last template run up to stackabletech/operator-templating@23454aeb350768655bb917a460e9d534d5d3acb9
    Reference-to: stackabletech/operator-templating@23454ae (Part of https://github.com/stackabletech/issues/issues/847)

--- scripts/docs_templating.sh
+++ scripts/docs_templating.sh
@@ -39,6 +39,8 @@ do
 done
 
 # Ensure this script is executable
-chmod +x docs/modules/opensearch/examples/getting_started/getting_started.sh
+chmod +x "docs/modules/spark-k8s-operator/examples/getting_started/getting_started.sh" \
+  || chmod +x "docs/modules/spark-k8s-operator/examples/getting_started/code/getting_started.sh" \
+  || true
 
 echo "done"
